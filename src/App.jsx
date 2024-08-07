fadc46928f21d6d2652350427220c2b6993c1f1b
code-with-sooraj <sauravuyadav003@gmail.com>
2024-08-07 09:50:21 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <iframe title="Ecommerce sales dashborad" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=97b31716-e2bf-41f7-ba6c-8ce6df110c45&autoAuth=true&ctid=3b0993d8-31db-4db6-b617-64ac193c7ace" frameborder="0" allowFullScreen="true"></iframe>
+    </>
+  )
+}
+
+export default App
